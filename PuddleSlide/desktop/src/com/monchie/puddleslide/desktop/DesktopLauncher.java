e938bc71eefd60f1de2551fcbdc614bfb248be63
kalita09 <kalam_1992@hotmail.com>
2014-09-14 21:03:33 -0600

    proyecto vacio

---
+++ PuddleSlide/desktop/src/com/monchie/puddleslide/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.monchie.puddleslide.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.monchie.puddleslide.PSGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PSGame(), config);
+	}
+}
