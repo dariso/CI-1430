4b7c739f968d9f5a4ac5c0b606af041b3debd30b
Daniel Rivera Solano <dariso23@gmail.com>
2014-09-16 18:50:38 -0600

    Pantalla de Juego, Fondo y Gota Renderizadas en pantalla 800x480 con ortographic camera

--- PuddleSlide/core/src/com/monchie/puddleslide/PSGame.java
+++ PuddleSlide/core/src/com/monchie/puddleslide/PSGame.java
@@ -3,25 +3,45 @@ package com.monchie.puddleslide;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class PSGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture gotaImage;
+    private Texture backgroundImage;
+    private OrthographicCamera camera;
+    private Vector3 touchPos;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		gotaImage = new Texture("gotty.png");
+        backgroundImage = new Texture("background.png");
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        touchPos = new Vector3();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0,0,0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
+
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+
+
+        batch.begin();
+        batch.draw(backgroundImage,0,0);
+		batch.draw(gotaImage,(800/2-64/2),(480/2-64/2));
 		batch.end();
 	}
 }
