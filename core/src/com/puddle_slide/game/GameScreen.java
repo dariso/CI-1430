ad3b7fb716bdf9347ac39c944f42abc161c8f37d
Daniel <dariso23@gmail.com>
2014-09-21 00:34:38 -0600

    Arreglo a Box2d

--- core/src/com/puddle_slide/game/GameScreen.java
+++ core/src/com/puddle_slide/game/GameScreen.java
@@ -46,7 +46,7 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         camera.update();
-        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClearColor(0,0,1f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(camera.combined);
 
