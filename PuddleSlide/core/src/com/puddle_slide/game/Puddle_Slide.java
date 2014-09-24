762f9db46568ab7e22e9e954b0eb4d1d82763fcd
MeliGarro <melissagm08@gmail.com>
2014-09-23 18:58:13 -0600

    Pantalla inicio conectada

--- PuddleSlide/core/src/com/puddle_slide/game/Puddle_Slide.java
+++ PuddleSlide/core/src/com/puddle_slide/game/Puddle_Slide.java
@@ -10,27 +10,24 @@ public class Puddle_Slide extends Game {
     SpriteBatch batch;
     BitmapFont font;
     private Vector3 touchPos;
+    public MainMenuScreen menu;//Agregado
 
     @Override
     public void create () {
-
         batch = new SpriteBatch();
         touchPos = new Vector3();
         font = new BitmapFont();
         this.setScreen(new MainMenuScreen(this));
+        this.setScreen(new MainStartScreen(this, menu));
     }
 
     @Override
     public void render () {
-
         super.render();
-
     }
 
     public void dispose() {
-
         batch.dispose();
         font.dispose();
-
     }
 }
