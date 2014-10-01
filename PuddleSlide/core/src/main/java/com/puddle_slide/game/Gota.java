150fa86acea6bca29617829299831ded8191c1d7
Elexia12 <xiannie.rivas@gmail.com>
2014-09-30 19:48:14 -0600

    Modularización de los objetos de juego

---
+++ PuddleSlide/core/src/main/java/com/puddle_slide/game/Gota.java
@@ -0,0 +1,48 @@
+package com.puddle_slide.game;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by xia on 9/30/14.
+ */
+public class Gota {
+
+    private Body gotaBody;
+
+    /**
+     * Constructor de la gota
+     * @param world Mundo en el que se dibujara la gota
+     * @param x Posicion en el eje x en el que se dibujara la gota
+     * @param y Posicion en el eje y en el que se dibujara la gota
+     * @param ancho Ancho del cuerpo de la gota
+     * */
+    public Gota(World world, float x, float y, float ancho){
+        BodyDef gotaDef = new BodyDef();
+        gotaDef.type = BodyDef.BodyType.DynamicBody;
+        gotaDef.position.set(x, y);
+        gotaBody = world.createBody(gotaDef);
+
+        CircleShape gotaShape = new CircleShape();
+        gotaShape.setRadius(ancho/2);                   //Para que sea el radio dividimos el ancho (diametro) por 2
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = gotaShape;
+        fixtureDef.density = 999.97f;
+        fixtureDef.friction = 0.2f;
+        fixtureDef.restitution = 0.1f;
+        gotaBody.createFixture(fixtureDef);
+        gotaShape.dispose();
+
+    }
+
+    public float getX(){
+        return gotaBody.getPosition().x;
+    }
+
+    public float getY(){
+        return gotaBody.getPosition().y;
+    }
+}
---
+++ PuddleSlide/core/src/main/java/com/puddle_slide/game/Hoja.java
@@ -0,0 +1,58 @@
+package com.puddle_slide.game;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by xia on 9/30/14.
+ */
+public class Hoja {
+
+    private Body hojaBody;
+
+    /**
+     * Constructor de la hoja
+     * @param world Mundo en el que se dibujara la hoja
+     * @param x Posicion en el eje x en el que se dibujara la hoja
+     * @param y Posicion en el eje y en el que se dibujara la hoja
+     * @param ancho Ancho del sprite de la hoja
+     * @param alto Alto del sprite de la hoja
+     * */
+    public Hoja(World world, float x, float y, float ancho, float alto){
+        BodyDef hojaDef = new BodyDef();
+        hojaDef.type = BodyDef.BodyType.DynamicBody;
+        hojaDef.position.set(x, y);
+        hojaBody = world.createBody(hojaDef);
+
+        PolygonShape hojaShape = new PolygonShape();
+        hojaShape.setAsBox(ancho-64, alto/4);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = hojaShape;
+        fixtureDef.density = 1000f;
+        fixtureDef.friction = 0.42f;
+        fixtureDef.restitution = 0.5f;
+        hojaBody.createFixture(fixtureDef);
+        hojaShape.dispose();
+    }
+
+    public float getX(){
+        return hojaBody.getPosition().x;
+    }
+
+    public float getY(){
+        return hojaBody.getPosition().y;
+    }
+
+    public void mover(Vector2 v){
+        hojaBody.applyForceToCenter(v, true);
+    }
+
+    public float getMasa(){
+        return hojaBody.getMass();
+    }
+}
